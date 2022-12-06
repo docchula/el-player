c6b9baba5d2ed06bc178f89d47f7e19635d20310
Siwat Techavoranant <keen@keendev.net>
2022-12-06 16:49:37 +0700

    Implemented all functions from v1

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,83 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
+import Player from './components/Player.vue';
+import {onMounted, ref} from 'vue';
+import {SunIcon} from '@heroicons/vue/20/solid';
+
+const url = ref<string | null>(null);
+const processUrl = (rawInput: string) => {
+  let input = null;
+  if (rawInput && isValidHttpUrl(rawInput)) {
+    // Transform URL input
+    if (rawInput.includes('.mp4')) {
+      input = rawInput;
+    } else {
+      const urlInput = new URL(rawInput);
+      if (rawInput.includes('aculearn-idm/')) {
+        input = 'http://cdn.md.chula.ac.th/content/' + urlInput.searchParams.get('author') + '/' + urlInput.searchParams.get('modulename') + '/media/' + (rawInput.includes('/v7/') ? '2' : '1') + '.mp4';
+      } else if (rawInput.includes('aculearn-me/')) {
+        input = 'http://cdn1.md.chula.ac.th/content/' + urlInput.searchParams.get('author') + '/' + urlInput.searchParams.get('modulename') + '/media/' + (rawInput.includes('/v7/') ? '2' : '1') + '.mp4';
+      } else {
+        alert('Error: Cannot extract video address!');
+      }
+    }
+  }
+
+  url.value = input;
+  if (url.value) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+    if (rawInput) {
+      alert('Invalid URL');
+    }
+  }
+};
+const toggleDarkMode = () => {
+  document.documentElement.classList.toggle('dark');
+};
+const isValidHttpUrl = (string: string) => {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+};
+onMounted(() => {
+  if (window.location.href.includes('aculearn-')) {
+    processUrl(window.location.href);
+  }
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-50 dark:bg-gray-900 sm:items-center sm:pt-0">
+    <div class="fixed top-0 right-0 px-6 py-4 block font-thin">
+      <a href="http://e-learning.md.chula.ac.th" target="_blank" class="text-sm text-gray-500 dark:text-gray-400 underline">MDCU E-Learning</a>
+    </div>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 mt-8 sm:mt-0">
+      <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+        <h1 class="text-6xl font-extrabold">
+          <span class="text-transparent bg-clip-text bg-gradient-to-br from-emerald-300 dark:from-emerald-200 to-green-700 dark:to-green-600">Docchula</span>
+          <span class="text-4xl">&ensp;</span>
+          <span class="text-gray-700 dark:text-gray-300">Video Player</span>
+        </h1>
+      </div>
+
+      <div class="mt-8">
+        <Player v-if="url" :src="url" @back="processUrl(null)"/>
+        <Home v-else :url="url" @submit="processUrl"/>
+      </div>
+
+      <div class="mt-20 mb-4 text-xs text-extralight text-gray-400 text-center">
+        Created with ❤ by Keen | <a href="https://github.com/docchula/el-player" target="_blank"
+                                    class="text-slate-300 dark:text-slate-500 hover:underline hover:text-gray-400 dark:hover:text-gray-300">Source Code</a> |
+        <SunIcon @click="toggleDarkMode" title="Toggle dark theme"
+                 class="inline-block h-5 cursor-pointer text-slate-300 dark:text-slate-500 hover:underline hover:text-gray-400 dark:hover:text-gray-300"/>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Player.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import {ChevronLeftIcon} from '@heroicons/vue/24/solid';
+import {onMounted, onBeforeUnmount, ref} from 'vue';
+import videojs from 'video.js';
+import "video.js/dist/video-js.css";
+import 'videojs-hotkeys';
+import 'videojs-seek-buttons';
+import 'videojs-seek-buttons/dist/videojs-seek-buttons.css';
+
+defineEmits(['back']);
+const props = defineProps<{
+  src: string;
+}>();
+const playerEl = ref<Element | null>(null);
+let player: videojs.Player;
+let startTime: number | null;
+let ticker: number | undefined;
+const startClock = ref<String | null>(null);
+onMounted(() => {
+  player = videojs(playerEl.value!, {
+    aspectRatio: "1280:640",
+    playbackRates: [0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5],
+    plugins: {
+      seekButtons: {forward: 10, back: 10}
+    }
+  }, () => {
+    player.hotkeys({
+      volumeStep: 0.1,
+      seekStep: 5,
+      enableModifiersForNumbers: false,
+      enableVolumeScroll: false
+    });
+    player.on('play', () => {
+      if (!startTime) {
+        startTime = (new Date()).getTime();
+        ticker = setInterval(() => {
+          const currentTime = (new Date()).getTime();
+          const diff = currentTime - startTime!;
+          const h = Math.floor(diff / 3600000);
+          const m = Math.floor((diff % 3600000) / 60000);
+          startClock.value = `${h}:${m.toString().padStart(2, '0')}`;
+        }, 10000);
+      }
+    });
+  });
+});
+onBeforeUnmount(() => {
+  if (player) {
+    player.dispose();
+  }
+  if (ticker) {
+    clearInterval(ticker);
+  }
+});
+const promptPlaybackSpeed = () => {
+  const speed = prompt('Specify playback speed (between 0.2-5.0)', player.playbackRate().toString());
+  if (speed) {
+    const speedNum = parseFloat(speed);
+    if (speedNum > 5 || speedNum < 0.2) {
+      alert('Invalid speed!');
+    } else {
+      player.playbackRate(speedNum);
+    }
+  }
+};
+</script>
+
+<template>
+  <video ref="playerEl" class="video-js vjs-default-skin w-max" controls preload="metadata">
+    <source :src="props.src" type="video/mp4">
+    Your browser does not support the video tag.
+  </video>
+  <div class="my-4 text-gray-500 dark:text-gray-400">
+    <p class="my-2 hidden md:block text-gray-400 dark:text-gray-500"><b>Hotkeys</b>&emsp; Space: Pause, ▲/▼: Volume, ◄/►: Seek, F: Fullscreen</p>
+    <span v-if="startClock">Time spent on this page: {{ startClock }} | </span><a class="cursor-pointer" @click="promptPlaybackSpeed">Set playback speed</a>
+  </div>
+  <div class="tracking-wide text-center text-green-600 mt-6">
+    <a class="cursor-pointer" @click="$emit('back')">
+      <ChevronLeftIcon class="inline-block h-5"/>
+      Back</a>
+  </div>
+</template>
