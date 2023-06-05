d66aafd393647997115bdb44ac565a45fed0c50c
Siwat Techavoranant <keen@keendev.net>
2023-06-06 00:15:51 +0700

    refactor: use Vue composition api instead of option api

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,83 +1,81 @@
-<script lang="ts">
+<script lang="ts" setup>
 import { ArrowPathIcon, PlayIcon, StopIcon } from '@heroicons/vue/24/solid';
+import { reactive, ref } from 'vue';
 
-export default {
-  data() {
-    return {
-      studyDuration: 25,
-      breakDuration: 5,
-      longBreakDuration: 20,
-      isTimerRunning: false,
-      open: false,
-      timeLeft: 0,
-      timerId: 0,
-      count: 0,
-      title: 'Timer',
-    };
-  },
-  methods: {
-    startTimer() {
-      if (this.studyDuration >= 1 && this.breakDuration >= 1) {
-        if (this.count === 0) {
-          this.title = 'Study';
-          this.count++;
-        }
-        this.isTimerRunning = true;
-        this.timeLeft =
-          this.count % 2 === 0
-            ? this.count % 3 === 0
-              ? this.longBreakDuration * 60
-              : this.breakDuration * 60
-            : this.studyDuration * 60;
-        this.timerId = setInterval(() => {
-          if (this.timeLeft > 0) {
-            this.timeLeft--;
-          } else {
-            this.stopTimer();
-            this.count++;
-            if (this.count % 2 === 0) {
-              if (this.count % 3 === 0) {
-                this.startTimer();
-                this.title = 'Long Break';
-                alert('Study time is up! Take a long break.');
-              } else {
-                this.startTimer();
-                this.title = 'Break';
-                alert('Study time is up! Take a break.');
-              }
-            } else {
-              this.startTimer();
-              this.title = 'Study';
-              alert('Break time is up! Get back to study.');
-            }
-          }
-        }, 1000);
+const settings = reactive({
+  studyDuration: 25,
+  breakDuration: 5,
+  longBreakDuration: 20,
+  isTimerRunning: false,
+  timerId: 0,
+  count: 0,
+  title: 'Timer',
+});
+const isOpen = ref(false);
+const timeLeft = ref<number>(0);
+
+const startTimer = () => {
+  if (settings.studyDuration >= 1 && settings.breakDuration >= 1) {
+    if (settings.count === 0) {
+      settings.title = 'Study';
+      settings.count++;
+    }
+    settings.isTimerRunning = true;
+    timeLeft.value =
+      settings.count % 2 === 0
+        ? settings.count % 3 === 0
+          ? settings.longBreakDuration * 60
+          : settings.breakDuration * 60
+        : settings.studyDuration * 60;
+    settings.timerId = setInterval(() => {
+      if (timeLeft.value > 0) {
+        timeLeft.value--;
       } else {
-        alert('Please provide durations that are at least 1 minute.');
+        stopTimer();
+        settings.count++;
+        if (settings.count % 2 === 0) {
+          if (settings.count % 3 === 0) {
+            startTimer();
+            settings.title = 'Long Break';
+            alert('Study time is up! Take a long break.');
+          } else {
+            startTimer();
+            settings.title = 'Break';
+            alert('Study time is up! Take a break.');
+          }
+        } else {
+          startTimer();
+          settings.title = 'Study';
+          alert('Break time is up! Get back to study.');
+        }
       }
-    },
-    stopTimer() {
-      this.isTimerRunning = false;
-      clearInterval(this.timerId);
-    },
-    resetTimer() {
-      this.stopTimer();
-      this.timeLeft = 0;
-    },
-    formatTime(time: number) {
-      const displayTime = new Date(0, 0);
-      displayTime.setSeconds(+time);
-      return displayTime.toTimeString().slice(0, 8);
-    },
-  },
-  components: { PlayIcon, StopIcon, ArrowPathIcon },
+    }, 1000);
+  } else {
+    alert('Please provide durations that are at least 1 minute.');
+  }
+};
+
+const stopTimer = () => {
+  settings.isTimerRunning = false;
+  clearInterval(settings.timerId);
+};
+
+const resetTimer = () => {
+  stopTimer();
+  timeLeft.value = 0;
+};
+
+const formatTime = (time: number) => {
+  const displayTime = new Date(0, 0);
+  displayTime.setSeconds(+time);
+  return displayTime.toTimeString().slice(0, 8);
 };
 </script>
 
 <template>
   <button
     class="cursor-pointer relative text-sm text-gray-500 dark:text-gray-400 block"
-    @click="open = !open"
+    @click="isOpen = !isOpen"
   >
     Pomodoro Timer
   </button>
@@ -90,11 +88,11 @@ export default {
     leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
   >
     <div
-      v-if="open"
+      v-if="isOpen"
       class="box-border p-4 mt-2 rounded-lg bg-gray-200 transition duration-1000 dark:bg-gray-800 block text-sm text-gray-700 dark:text-gray-500"
     >
       <div class="text-center">
-        <h2 class="text-lg font-bold">{{ title }}</h2>
+        <h2 class="text-lg font-bold">{{ settings.title }}</h2>
         <div class="text-xl">
           {{ formatTime(timeLeft) }}
         </div>
@@ -117,34 +115,34 @@ export default {
         <div>
           <label class="label">Study Duration (minutes)</label>
           <input
+            v-model.number="settings.studyDuration"
             class="block w-full rounded-md border-gray-300 focus:border-green-400 transition duration-500 dark:focus:border-green-800 focus:ring-green-400 dark:focus:ring-green-800 sm:text-sm dark:bg-gray-800 dark:text-gray-200"
-            type="number"
             min="1"
-            placeholder="25"
             onkeypress="return (event.key >= '0' && event.key <= '9') || event.key === '.'"
-            v-model.number="studyDuration"
+            placeholder="25"
+            type="number"
           />
         </div>
         <div>
           <label class="label">Break Duration (minutes)</label>
           <input
+            v-model.number="settings.breakDuration"
             class="block w-full rounded-md border-gray-300 focus:border-green-400 transition duration-500 dark:focus:border-green-800 focus:ring-green-400 dark:focus:ring-green-800 sm:text-sm dark:bg-gray-800 dark:text-gray-200"
-            type="number"
             min="1"
-            placeholder="5"
             onkeypress="return (event.key >= '0' && event.key <= '9') || event.key === '.'"
-            v-model.number="breakDuration"
+            placeholder="5"
+            type="number"
           />
         </div>
         <div>
           <label class="label">Long Break Duration (minutes)</label>
           <input
+            v-model.number="settings.longBreakDuration"
             class="block w-full rounded-md border-gray-300 focus:border-green-400 transition duration-500 dark:focus:border-green-800 focus:ring-green-400 dark:focus:ring-green-800 sm:text-sm dark:bg-gray-800 dark:text-gray-200"
-            type="number"
             min="1"
-            placeholder="15"
             onkeypress="return (event.key >= '0' && event.key <= '9') || event.key === '.'"
-            v-model.number="longBreakDuration"
+            placeholder="15"
+            type="number"
           />
         </div>
       </div>
