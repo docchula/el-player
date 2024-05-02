3d378c9a91d7c8178a5591feb94f0094126a9301
Ittipat <39715559+ittipatken@users.noreply.github.com>
2024-05-03 01:58:17 +0700

    feat: remove Songkran theme (#24)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,12 @@ module.exports = {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'special-day-light': "url('/bg-songkran-light.webp')",
-        'special-day-dark': "url('/bg-songkran-dark.webp')",
-      }
-    },
+    // extend: {
+    //   backgroundImage: {
+    //     'special-day-light': "url('/')",
+    //     'special-day-dark': "url('/')",
+    //   }
+    // },
   },
   plugins: [require('@tailwindcss/forms')],
 };
