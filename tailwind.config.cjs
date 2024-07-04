365050f22fccf7ba27d3768247baa32a2343a752
Ittipat <39715559+ittipatken@users.noreply.github.com>
2024-07-04 12:01:02 +0700

    feat: Buddhism theme (#27)
    * feat: add buddhism theme
    
    * feat: change text colors
    
    * feat: change backgrounds
    
    * feat: change backgrounds
    
    * fix: text color is hidden

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,12 @@ module.exports = {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    // extend: {
-    //   backgroundImage: {
-    //     'special-day-light': "url('/')",
-    //     'special-day-dark': "url('/')",
-    //   }
-    // },
+    extend: {
+      backgroundImage: {
+        'special-day-light': "url('/bg-buddhism-light.webp')",
+        'special-day-dark': "url('/bg-buddhism-dark.webp')",
+      }
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
